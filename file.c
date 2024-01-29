f1e54097bf97f6de310d18d40941237cca61c7f0
forzalazio69 <158043348+forzalazio69@users.noreply.github.com>
2024-01-29 15:48:34 +0000

    lezione 1

---
+++ file.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+   long x = 151548533987428268;
+
+   printf("luca non se lava da %ld anni", x);
+    return(0);
+}
---
+++ file1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    printf ("quale è l'età totale della nostra fila?\n");
+    printf ("scrivi la prima età\n");
+    int numero3; 
+    scanf ("%d", &numero3);
+    printf("scrivi la seconda età\n");
+    int numero1; 
+    scanf("%d", &numero1);
+    printf("scrivi la terza età\n");
+    int numero2; 
+    scanf("%d", &numero2);
+    printf("scrivi la quarta età\n");
+    int numero4; 
+    scanf("%d", &numero4);
+    int x = numero1 + numero2 + numero3 + numero4;
+    printf ("la somma è  %d\n", x);
+    return(0);
+}
